be404002f612dae52521f4c62303568ff4732e4a
bigeast <dadongking@gmail.com>
2017-04-13 17:08:45 +0800

    brute force

--- PE571.cc
+++ PE571.cc
@@ -3,7 +3,14 @@
 #include <string>
 #include <iostream>
 using namespace std;
+#define DIV_FUN 0   // div function is even slower than '% then /'.
+#define OPT_248 1   // this is a big improvement.
+#if OPT_248
 unsigned int bases[] = { 2,4,8,3,5,6,7,9,10,11 };
+#else
+unsigned int bases[] = { 2,3,4,5,6,7,8,9,10,11 };
+#endif
+#if DIV_FUN
 bool check(unsigned long long n) {
     unsigned int mask, base;
     long long num;
@@ -27,19 +34,45 @@ bool check(unsigned long long n) {
     }
     return res;
 }
+#else
+bool check(unsigned long long n) {
+    unsigned int mask, base;
+    long long num;
+    bool res = true;
+    size_t len = sizeof(bases) / sizeof(bases[0]);
+    for(int idx = 0; idx < len; ++idx){
+        base = bases[idx];
+        num = n;
+        mask = 0;
+        while(num) {
+            mask |= (1 << (num % base));
+            num /= base;
+            if(mask == (1 << base) - 1) break; // already fit.
+        }
+        if(mask != (1 << base) - 1) {
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
+#endif
 int main() {
-    string str("0123456789ab");
+    //string str("0123456789ab");
+    char str[]="0123456789ab";
     unsigned long long n, res = 0;
     int cnt = 0;
     do{
-        n = strtoll(str.c_str(), nullptr, 12);
+        n = strtoll(str, nullptr, 12);
         if(check(n)) {
             ++cnt;
             cout<<cnt<<": "<<n<<endl;
             res += n;
-            if(cnt == 10) return 0;
+            if(cnt == 10) {
+                cout << "res is: " << res << endl;
+                return 0;
+            }
         }
-    } while(next_permutation(str.begin(), str.end()));
-    cout << "res is: " << res << endl;
+    } while(next_permutation(str, str+12));
     return 0;
 }
